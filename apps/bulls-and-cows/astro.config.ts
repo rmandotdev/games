000bbf6fa3823d100209c8df472697fd0586f26b
Roman <dev@rman.dev>
2026-07-23 14:16:16 +0100

    Update astro.config.ts

--- apps/bulls-and-cows/astro.config.ts
+++ apps/bulls-and-cows/astro.config.ts
@@ -1,9 +1,10 @@
 import solid from "@astrojs/solid-js";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "static",
   integrations: [solid()],
-  vite: { build: { cssTarget: "safari15" } },
+  vite: { plugins: [tailwindcss()], build: { cssTarget: "safari15" } },
   site: "https://bulls-and-cows.rman.dev",
 });

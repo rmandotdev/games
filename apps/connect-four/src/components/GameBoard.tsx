6b514642e4b74fe3b04edd3f4d6f921f1a43c30e
Roman <dev@rman.dev>
2026-07-23 22:50:45 +0100

    fix

--- apps/connect-four/src/components/GameBoard.tsx
+++ apps/connect-four/src/components/GameBoard.tsx
@@ -14,7 +14,7 @@ function GameBoard(props: {
   return (
     <div class="flex w-full flex-col items-center">
       {/* must not have gap, otherwise clicking between cells will be broken */}
-      <div class="grid aspect-7/6 w-game-board max-w-full grid-cols-7 grid-rows-6 rounded-5 bg-board p-2 sm:p-4 md:w-150 lg:w-187.5 xl:w-225">
+      <div class="grid aspect-7/6 height-600:w-game-board-600 height-700:w-game-board-700 height-800:w-game-board-800 w-game-board max-w-full grid-cols-7 grid-rows-6 rounded-5 bg-board p-2 sm:p-4 md:w-150">
         <For each={props.board}>
           {(row, rowIndex) => (
             <For each={row}>

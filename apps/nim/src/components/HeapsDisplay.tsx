9d061b36bcd9e8f7139074260f12533435420e76
Roman <dev@rman.dev>
2026-07-25 00:34:24 +0100

    chore(nim): fully use tailwindcss

--- apps/nim/src/components/HeapsDisplay.tsx
+++ apps/nim/src/components/HeapsDisplay.tsx
@@ -34,7 +34,7 @@ function HeapsDisplay(props: HeapsDisplayProps) {
             <div class="mb-4 inline-block rounded-lg bg-black/20 px-4 py-2 text-lg text-primary">
               <span>{count}</span> stones
             </div>
-            <div class="stones grid content-start rounded-lg bg-black/20 p-4">
+            <div class="grid h-stones-h w-stones-w grid-cols-stones content-start gap-stones-gap rounded-lg bg-black/20 p-4">
               <For each={Array(count).fill(0)}>
                 {() => (
                   <div class="relative size-stone rounded-full bg-gradient-stone shadow-2-4-20 transition-all duration-300 ease-out" />
